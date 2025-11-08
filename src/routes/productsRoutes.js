a7463aa2f69e9f8435b753904260a982b448f0ea
Alexander Hadion <gadion_alex@ukr.net>
2025-11-08 12:27:47 +0200

    added putch

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createProduct, getAllProducts,getProductById } from "../controllers/productsController.js";
+import { createProduct, getAllProducts,getProductById, updateProduct } from "../controllers/productsController.js";
 
 const router = Router();
 
 router.get('/products', getAllProducts);
 router.get("/products/:productId", getProductById);
-router.post("/products",createProduct);
+router.post("/products", createProduct);
+router.patch('/products/:productId', updateProduct);
 
 export default router;
