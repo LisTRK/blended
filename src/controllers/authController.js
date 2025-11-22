fc51312a573bb0c34a3c577af8510fdc86bf6571
Lyubov9302 <lyubovyurynets@gmail.com>
2025-11-22 17:05:55 +0700

    added login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,8 @@
 import createHttpError from "http-errors";
 import { User } from "../models/user.js";
 import bcrypt from 'bcrypt';
+import { createSession, setSessionCookies } from "../services/auth.js";
+import { Session } from "../models/session.js";
 
 export const userRegister = async (req, res) => {
   const { name, email, password } = req.body;
@@ -20,4 +22,25 @@ export const userRegister = async (req, res) => {
   })
 
   res.status(201).json(newUser)
+}
+
+export const userLogin = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw createHttpError(401, "Invalid user");
+  }
+
+  const isValidPassword = await bcrypt.compare(password, user.password);
+  if (!isValidPassword) {
+    throw createHttpError(401, "Invalid user");
+  }
+
+  await Session.deleteOne({ userId: user._id});
+  const newSession = await createSession(user._id);
+
+  setSessionCookies(res, newSession);
+
+  res.status(200).json(user);
 }
