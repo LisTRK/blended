84b9bc764454d1a22a57f4120390e5e78304adfb
Alexander Hadion <gadion_alex@ukr.net>
2025-11-22 12:22:49 +0200

    added logout

--- src/routes/authRouts.js
+++ src/routes/authRouts.js
@@ -1,13 +1,14 @@
 import { celebrate } from "celebrate";
 import { Router } from "express";
 import { loginUserSchema, registerUserSchema } from "../validations/authValidation.js";
-import { userLogin, userRegister } from "../controllers/authController.js";
+import { userLogin, userLogout, userRegister } from "../controllers/authController.js";
 
 
 const router = Router();
 
 router.post("/auth/register", celebrate(registerUserSchema), userRegister);
 router.post("/auth/login", celebrate(loginUserSchema), userLogin);
+router.post('/auth/logout', userLogout);
 
 export default router;
 
